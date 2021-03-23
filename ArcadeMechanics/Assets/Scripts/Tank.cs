141e4b0fa0a0560b645d8b71f3119ba1a55a6616
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2021-03-23 12:34:01 +0100

    Added bullets

--- ArcadeMechanics/Assets/Scripts/Tank.cs
+++ ArcadeMechanics/Assets/Scripts/Tank.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Tank : MonoBehaviour
 {
+    public Transform bulletSpawnPoint;
+
+    public GameObject bullet;
+
+    public float bulletForce = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,22 @@ public class Tank : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //For testing
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject newBullet = Instantiate(bullet, bulletSpawnPoint.position, Quaternion.identity);
+
+        Rigidbody2D newBulletRB = newBullet.GetComponent<Rigidbody2D>();
+
+        //newBulletRB.AddForce(transform.forward * bulletForce);
+        newBulletRB.velocity = new Vector2(bulletForce, 0);
+
+        Destroy(newBullet, 5f);
     }
 }
