e36ceaa887965f63195a9ebf42cb9187fc68ad53
Bart <Bartdb2001@gmail.com>
2021-03-23 12:35:26 +0100

    very basic leaderboard

--- ArcadeMechanics/Assets/Scripts/Leaderboard.cs
+++ ArcadeMechanics/Assets/Scripts/Leaderboard.cs
@@ -9,25 +9,25 @@ public class Leaderboard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        leaderboardScores[0].text = "100";
-        leaderboardScores[1].text = "CPU";
-        leaderboardScores[2].text = "90";
-        leaderboardScores[3].text = "SYM";
-        leaderboardScores[4].text = "80";
-        leaderboardScores[5].text = "LVD";
-        leaderboardScores[6].text = "70";
-        leaderboardScores[7].text = "DAN";
-        leaderboardScores[8].text = "60";
-        leaderboardScores[9].text = "BDB";
-        leaderboardScores[10].text = "50";
+        leaderboardScores[0].text = "Scores:";
+        leaderboardScores[1].text = "Names:";
+        leaderboardScores[2].text = "100";
+        leaderboardScores[3].text = "LVD";
+        leaderboardScores[4].text = "90";
+        leaderboardScores[5].text = "DAN";
+        leaderboardScores[6].text = "80";
+        leaderboardScores[7].text = "BDB";
+        leaderboardScores[8].text = "70";
+        leaderboardScores[9].text = "SYM";
+        leaderboardScores[10].text = "60";
         leaderboardScores[11].text = "KLK";
-        leaderboardScores[12].text = "40";
+        leaderboardScores[12].text = "50";
         leaderboardScores[13].text = "SD1";
-        leaderboardScores[14].text = "30";
-        leaderboardScores[15].text = "TF2";
-        leaderboardScores[16].text = "20";
-        leaderboardScores[17].text = "SCH";
-        leaderboardScores[18].text = "10";
+        leaderboardScores[14].text = "40";
+        leaderboardScores[15].text = "SCH";
+        leaderboardScores[16].text = "30";
+        leaderboardScores[17].text = "FUN";
+        leaderboardScores[18].text = "20";
         leaderboardScores[19].text = "USB";
     }
 
