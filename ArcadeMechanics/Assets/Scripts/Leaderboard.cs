710066fa3861098a51c8366ad3821c3575ea91d4
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2021-03-23 12:52:40 +0100

    Changed Text to Textmeshpro

--- ArcadeMechanics/Assets/Scripts/Leaderboard.cs
+++ ArcadeMechanics/Assets/Scripts/Leaderboard.cs
@@ -1,34 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Leaderboard : MonoBehaviour
 {
-    public Text[] leaderboardScores;
+    public TextMeshProUGUI[] leaderboardScores;
+    public TextMeshProUGUI[] leaderboardNames;
     // Start is called before the first frame update
     void Start()
     {
         leaderboardScores[0].text = "Scores:";
-        leaderboardScores[1].text = "Names:";
-        leaderboardScores[2].text = "100";
-        leaderboardScores[3].text = "LVD";
-        leaderboardScores[4].text = "90";
-        leaderboardScores[5].text = "DAN";
-        leaderboardScores[6].text = "80";
-        leaderboardScores[7].text = "BDB";
-        leaderboardScores[8].text = "70";
-        leaderboardScores[9].text = "SYM";
-        leaderboardScores[10].text = "60";
-        leaderboardScores[11].text = "KLK";
-        leaderboardScores[12].text = "50";
-        leaderboardScores[13].text = "SD1";
-        leaderboardScores[14].text = "40";
-        leaderboardScores[15].text = "SCH";
-        leaderboardScores[16].text = "30";
-        leaderboardScores[17].text = "FUN";
-        leaderboardScores[18].text = "20";
-        leaderboardScores[19].text = "USB";
+        leaderboardNames[0].text = "Names:";
+
+        leaderboardScores[1].text = "100";
+        leaderboardNames[1].text = "LVD";
+
+        leaderboardScores[2].text = "90";
+        leaderboardNames[2].text = "DAN";
+
+        leaderboardScores[3].text = "80";
+        leaderboardNames[3].text = "BDB";
+
+        leaderboardScores[4].text = "70";
+        leaderboardNames[4].text = "SYM";
+
+        leaderboardScores[5].text = "60";
+        leaderboardNames[5].text = "KLK";
+
+        leaderboardScores[6].text = "50";
+        leaderboardNames[6].text = "SD1";
+
+        leaderboardScores[7].text = "40";
+        leaderboardNames[7].text = "SCH";
+
+        leaderboardScores[8].text = "30";
+        leaderboardNames[8].text = "FUN";
+
+        leaderboardScores[9].text = "20";
+        leaderboardNames[9].text = "USB";
     }
 
     // Update is called once per frame
