38e9795c7c19259edd6976b1b8089ad6c964a1be
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2021-03-30 12:21:03 +0200

    Added leaderboard update

--- ArcadeMechanics/Assets/Scripts/Leaderboard.cs
+++ ArcadeMechanics/Assets/Scripts/Leaderboard.cs
@@ -11,121 +11,86 @@ public class Leaderboard : MonoBehaviour
     public TextMeshProUGUI[] leaderboardNames;
     public int highscore;
 
-    /*
+    public int maxLeaderboardSpots = 10;
+
+    [HideInInspector] public Score[] leaderboard;
+    
     public class Score
     {
-        public int playerscore;
-        public string playername;
+        public int score;
+        public string name;
+
+        public Score(int _score, string _name)
+        {
+            score = _score;
+            name = _name;
+        }
     }
-    */
-    public void updateLeaderboard(int lastscore, string inputname)
+
+    public void UpdateLeaderboard(int newScore, string newName)
+    {
+        Score currentScore = new Score(newScore, newName);
+
+        for(int i = 0; i < leaderboard.Length; i++)
+        {
+            if(currentScore.score > leaderboard[i].score)
+            {
+                //Remove last and move everything
+                for (int j = maxLeaderboardSpots - 1; j >= i; j--)
+                {
+                    int tmpIndex = j - 1;
+                    if(tmpIndex >= 0)
+                    {
+                        leaderboard[j] = new Score(0, "");
+                        Score tmp = leaderboard[tmpIndex];
+                        leaderboard[j] = tmp;
+                    }
+                }
+
+                //Add new score
+                leaderboard[i] = currentScore;
+
+                break;
+            }
+        }
+
+        SaveLeaderboard();
+    }
+
+    public void SaveLeaderboard()
+    {
+        for(int i = 0; i < leaderboard.Length; i++)
+        {
+            PlayerPrefs.SetInt("LeaderboardScore" + i, leaderboard[i].score);
+            PlayerPrefs.SetString("LeaderboardName" + i, leaderboard[i].name);
+        }
+
+        PlayerPrefs.Save();
+    }
+
+    public void LoadLeaderboard()
+    {
+        leaderboard = new Score[maxLeaderboardSpots];
+
+        for (int i = 0; i < leaderboard.Length; i++)
+        {
+            int score = PlayerPrefs.GetInt("LeaderboardScore" + i, 0);
+            string name = PlayerPrefs.GetString("LeaderboardName" + i, "");
+
+            leaderboard[i] = new Score(score, name);
+        }
+    }
+
+    public void UpdateLeaderboardOnScreen()
     {
 
     }
-
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(highscore);
-        leaderboardScores[0].text = "100";
-        leaderboardNames[0].text = "LVD";
+        LoadLeaderboard();
 
-        leaderboardScores[1].text = "90";
-        leaderboardNames[1].text = "DAN";
-
-        leaderboardScores[2].text = "80";
-        leaderboardNames[2].text = "BDB";
-
-        leaderboardScores[3].text = "70";
-        leaderboardNames[3].text = "SYM";
-
-        leaderboardScores[4].text = "60";
-        leaderboardNames[4].text = "KLK";
-
-        leaderboardScores[5].text = "50";
-        leaderboardNames[5].text = "SD1";
-
-        leaderboardScores[6].text = "40";
-        leaderboardNames[6].text = "SCH";
-
-        leaderboardScores[7].text = "30";
-        leaderboardNames[7].text = "FUN";
-
-        leaderboardScores[8].text = "20";
-        leaderboardNames[8].text = "USB";
-
-        //highscore = PlayerPrefs.GetInt("Highscore");
-        int score1 = Int32.Parse(leaderboardScores[0].text);
-        int score2 = Int32.Parse(leaderboardScores[1].text);
-        int score3 = Int32.Parse(leaderboardScores[2].text);
-        int score4 = Int32.Parse(leaderboardScores[3].text);
-        int score5 = Int32.Parse(leaderboardScores[4].text);
-        int score6 = Int32.Parse(leaderboardScores[5].text);
-        int score7 = Int32.Parse(leaderboardScores[6].text);
-        int score8 = Int32.Parse(leaderboardScores[7].text);
-        int score9 = Int32.Parse(leaderboardScores[8].text);
-
-        if (highscore >= score1)
-        {
-            score1 = highscore;
-            leaderboardScores[0].text = score1.ToString();
-            Debug.Log("Score 1 update");
-
-        }
-        else if (highscore <= score1 & highscore >= score2)
-        {
-            score2 = highscore;
-            leaderboardScores[1].text = score2.ToString();
-            Debug.Log("Score 2 update");
-        }
-        else if (highscore <= score2 & highscore >= score3)
-        {
-            score3 = highscore;
-            leaderboardScores[2].text = score3.ToString();
-            Debug.Log("Score 3 update");
-        }
-        else if (highscore <= score3 & highscore >= score4)
-        {
-            score4 = highscore;
-            leaderboardScores[3].text = score4.ToString();
-            Debug.Log("Score 4 update");
-        }
-        else if (highscore <= score4 & highscore >= score5)
-        {
-            score5 = highscore;
-            leaderboardScores[4].text = score5.ToString();
-            Debug.Log("Score 5 update");
-        }
-        else if (highscore <= score5 & highscore >= score6)
-        {
-            score6 = highscore;
-            leaderboardScores[5].text = score6.ToString();
-            Debug.Log("Score 6 update");
-        }
-        else if (highscore <= score6 & highscore >= score7)
-        {
-            score7 = highscore;
-            leaderboardScores[6].text = score7.ToString();
-            Debug.Log("Score 7 update");
-        }
-        else if (highscore <= score7 & highscore >= score8)
-        {
-            score8 = highscore;
-            leaderboardScores[7].text = score8.ToString();
-            Debug.Log("Score 8 update");
-        }
-        else if (highscore <= score8 & highscore >= score9)
-        {
-            score9 = highscore;
-            leaderboardScores[8].text = score9.ToString();
-            Debug.Log("Score 9 update");
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        UpdateLeaderboardOnScreen();
     }
 }
