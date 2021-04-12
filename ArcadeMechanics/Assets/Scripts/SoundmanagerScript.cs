1188b213fbc03f79d07ef2f904916b91c2556466
lucasvdiepen <lucascool@ziggo.nl>
2021-04-12 13:11:27 +0200

    Revert "This is a failed merge"
    This reverts commit b7fc15c5eb5d3015047da4a2254ed49bb497d531, reversing
    changes made to 3f8ece9bc61ba7ea99d62884b0608534bfb4f6c9.

--- ArcadeMechanics/Assets/Scripts/SoundmanagerScript.cs
+++ ArcadeMechanics/Assets/Scripts/SoundmanagerScript.cs
@@ -1,10 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< HEAD
-=======
-using UnityEngine.UI;
->>>>>>> development
 
 public class SoundmanagerScript : MonoBehaviour
 {
@@ -12,14 +8,9 @@ public class SoundmanagerScript : MonoBehaviour
     public AudioClip[] DeathSounds;
     public AudioClip[] BGM;
     private AudioSource source;
-<<<<<<< HEAD
     public float SoundsVolume = 0.5f;
     public float BGMVolume = 0.3f;
 
-=======
-    float BGMVolume = 0.3f;
-    float SFXVolume = 0.5f;
->>>>>>> development
 
 
     // Start is called before the first frame update
@@ -30,33 +21,22 @@ public class SoundmanagerScript : MonoBehaviour
         StartBGMMusic();
 
     }
-<<<<<<< HEAD
     // Update is called once per frame
     void Update()
     {
         
     }
-=======
->>>>>>> development
 
     public void PlayJumpSounds()
     {
         source.clip = JumpSounds [Random.Range(0, JumpSounds.Length)];
-<<<<<<< HEAD
         source.volume = SoundsVolume;
-=======
-        source.volume = SFXVolume;
->>>>>>> development
         source.PlayOneShot(source.clip);
     }
     public void PlayDeathSounds()
     {
         source.clip = DeathSounds[Random.Range(0, DeathSounds.Length)];
-<<<<<<< HEAD
         source.volume = SoundsVolume;
-=======
-        source.volume = SFXVolume;
->>>>>>> development
         source.PlayOneShot(source.clip);
     }
     public IEnumerator StartBGMMusic()
