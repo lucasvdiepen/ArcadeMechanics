23fda1da51751667566102bb279ec0b9e9dbeebd
lucasvdiepen <lucascool@ziggo.nl>
2021-04-12 13:11:05 +0200

    Still not working

--- ArcadeMechanics/Assets/Scripts/SoundmanagerScript.cs
+++ ArcadeMechanics/Assets/Scripts/SoundmanagerScript.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+<<<<<<< HEAD
+=======
+using UnityEngine.UI;
+>>>>>>> development
 
 public class SoundmanagerScript : MonoBehaviour
 {
@@ -8,9 +12,14 @@ public class SoundmanagerScript : MonoBehaviour
     public AudioClip[] DeathSounds;
     public AudioClip[] BGM;
     private AudioSource source;
+<<<<<<< HEAD
     public float SoundsVolume = 0.5f;
     public float BGMVolume = 0.3f;
 
+=======
+    float BGMVolume = 0.3f;
+    float SFXVolume = 0.5f;
+>>>>>>> development
 
 
     // Start is called before the first frame update
@@ -21,22 +30,33 @@ public class SoundmanagerScript : MonoBehaviour
         StartBGMMusic();
 
     }
+<<<<<<< HEAD
     // Update is called once per frame
     void Update()
     {
         
     }
+=======
+>>>>>>> development
 
     public void PlayJumpSounds()
     {
         source.clip = JumpSounds [Random.Range(0, JumpSounds.Length)];
+<<<<<<< HEAD
         source.volume = SoundsVolume;
+=======
+        source.volume = SFXVolume;
+>>>>>>> development
         source.PlayOneShot(source.clip);
     }
     public void PlayDeathSounds()
     {
         source.clip = DeathSounds[Random.Range(0, DeathSounds.Length)];
+<<<<<<< HEAD
         source.volume = SoundsVolume;
+=======
+        source.volume = SFXVolume;
+>>>>>>> development
         source.PlayOneShot(source.clip);
     }
     public IEnumerator StartBGMMusic()
