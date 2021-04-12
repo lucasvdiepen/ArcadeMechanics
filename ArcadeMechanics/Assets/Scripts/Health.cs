2d355e340bfa6e7eed281b853e1e3170e163ac30
Lucas van Diepen <lucascool@ziggo.nl>
2021-04-12 12:53:52 +0200

    Reverted to working version

--- ArcadeMechanics/Assets/Scripts/Health.cs
+++ ArcadeMechanics/Assets/Scripts/Health.cs
@@ -1,26 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< HEAD
 
 public class Health : MonoBehaviour
 {
     public int startingHealth = 100;
     public int health = 0;
 
-=======
-using UnityEngine.UI;
-
-public class Health : MonoBehaviour
-{
-    public Slider healthBar;
-
-    public int startingHealth = 100;
-    public int health = 0;
-
-    public bool isBoss = false;
-
->>>>>>> development
     private void Start()
     {
         health = startingHealth;
@@ -30,41 +16,15 @@ public class Health : MonoBehaviour
     {
         health -= damage;
 
-<<<<<<< HEAD
         if(health <= 0)
         {
             //Dead
             FindObjectOfType<GameManager>().Die();
-=======
-        healthBar.value = health;
-
-        if(health <= 0)
-        {
-            //Dead
-            if(!isBoss)
-            {
-                FindObjectOfType<GameManager>().Die();
-            }
-            else
-            {
-                //Remove enemy
-                FindObjectOfType<ObstacleManager>().BossKilled(transform.position);
-                Destroy(gameObject);
-            }
->>>>>>> development
         }
     }
 
     public void ResetHealth()
     {
-<<<<<<< HEAD
         health = startingHealth;
-=======
-        if(!isBoss)
-        {
-            health = startingHealth;
-            healthBar.value = health;
-        }
->>>>>>> development
     }
 }
