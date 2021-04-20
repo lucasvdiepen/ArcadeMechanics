3a59f559ee8e97d79ff6be575a54d804762c58ad
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2021-04-20 09:54:14 +0200

    Added main menu button in settings menu

--- ArcadeMechanics/Assets/UI/UIScripts/PauseScreen.cs
+++ ArcadeMechanics/Assets/UI/UIScripts/PauseScreen.cs
@@ -1,23 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PauseScreen : MonoBehaviour
 {
     public Button settingsButton;
     public Button backButton;
+    public Button mainMenuButton;
 
     private void OnEnable()
     {
         settingsButton.onClick.AddListener(delegate { SettingsButtonClicked(); });
         backButton.onClick.AddListener(BackButtonClicked);
+        mainMenuButton.onClick.AddListener(MainMenuButtonClicked);
     }
 
     private void OnDisable()
     {
         settingsButton.onClick.RemoveAllListeners();
         backButton.onClick.RemoveAllListeners();
+        mainMenuButton.onClick.RemoveAllListeners();
+    }
+
+    private void MainMenuButtonClicked()
+    {
+        FindObjectOfType<GameManager>().Pause();
+        SceneManager.LoadScene("MainScene");
     }
 
     private void SettingsButtonClicked()
